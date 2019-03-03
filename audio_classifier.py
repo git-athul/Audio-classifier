ba040b74f3ea92a71911ac3f28a9ff05ca83e10d
athul <kpathul@outlook.com>
2019-03-03 14:19:23 +0530

    Adds 'mp3_to_RAW()'
    Converts mp3 files to RAW audio files using 'pydub' module

--- audio_classifier.py
+++ audio_classifier.py
@@ -1,9 +1,20 @@
+import acoustid
 from os import listdir
+from os.path import join
+from pydub import AudioSegment
 
 def mp3files(path):
     "Returns mp3 files from the given directory"
     return [f for f in listdir(path) if ".mp3" in f]
 
+def mp3_to_RAW(mp3, audiopath):
+    "Converts mp3file to RAW audio file"
+    mp3path = join(audiopath, mp3)
+    return AudioSegment.from_mp3(mp3path)
+
 if __name__ == "__main__":
-    audiopath = "./audiodir"
-    print(mp3files(audiopath))
+    audio_path = "./audiodir"
+    audio_list = mp3files(audio_path)
+    for f in audio_list:
+        mp3_to_RAW(f, audio_path)
+        # print(mp3_to_RAW(f, audio_path))
