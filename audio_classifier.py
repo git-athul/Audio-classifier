6957a3a32ec98188a1202d9e60d6f71ce55b2aed
athul <kpathul@outlook.com>
2019-03-05 11:26:28 +0530

    Removes 'mp3_to_RAW()'
    It is not required to convert audio files into raw format,
    in order to fingerprint them.

--- audio_classifier.py
+++ audio_classifier.py
@@ -1,30 +1,22 @@
 import acoustid
-import requests
+import config   #contains API-key
 from os import listdir
 from os.path import join
-from pydub import AudioSegment
 
 def mp3files(path):
     "Returns mp3 files from the given directory."
     return [f for f in listdir(path) if ".mp3" in f]
 
-# Able to fingerprint without this function
-# def mp3_to_RAW(mp3, audiopath):
-#     "Converts mp3file to RAW audio file"
-#     mp3path = join(audiopath, mp3)
-#     return AudioSegment.from_mp3(mp3path)
-
 def process_file( file_name) :
-    "process a file, given file_name and returns fingerprint of that file."
+    "process a file and returns fingerprint and duration."
     (duration, fingerprint) = acoustid.fingerprint_file(file_name)
     return duration, fingerprint
 
 if __name__ == "__main__":
     audio_path = "./audiodir"
     audio_list = mp3files(audio_path)
-    dict_aud = {}
     for f in audio_list:
-        dur, fingerprint = process_file(join(audio_path,f ))
-        out = acoustid.lookup('2Shct5QtQi', fingerprint, dur)
+        dur, fp = process_file(join(audio_path,f ))
+        out = acoustid.lookup(config.apikey, fp, dur)
         print(out)
 
