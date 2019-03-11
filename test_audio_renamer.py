b13adbdec8937bea28a642b85d5ecefd5da8eabe
athul <kpathul@outlook.com>
2019-03-11 08:47:06 +0530

    Modifies 'test_mp3file()'
    Removes 'test_data/'

--- test_audio_renamer.py
+++ test_audio_renamer.py
@@ -1,35 +1,42 @@
+import unittest
+import mock
 import audio_renamer
 
-def test_mp3files():
-    rst = ["1.mp3"]
-    assert audio_renamer.mp3files('./test_data') == rst
-
+class TestMp3Files(unittest.TestCase):
+    @mock.patch('audio_renamer.os.listdir')
+    def test_mp3files(self, mock_listdir):
+        mock_listdir.return_value = ['a.mp3', 'b.mp3', 'c.mp3', 'd.txt']
+        files = audio_renamer.mp3files('.')
+        self.assertEqual(3, len(files))
+        self.assertNotEqual(2, len(files))
 
 def test_artists():
     data = [{'id': 'cc', 'name': 'Adele'}]
     rst = "Adele"
     assert audio_renamer.artists(data) == rst
 
-    data = [{'joinphrase': ' & ', 'name': 'Skrillex'}, {'joinphrase':
-            ' feat. ', 'name': 'Diplo'}, {'name': 'Justin Bieber'}]
-    rst ="Skrillex & Diplo feat. Justin Bieber"
+    data = [{'joinphrase': ' & ', 'name': 'Skrillex'},
+            {'joinphrase':' feat. ', 'name': 'Diplo'},
+            {'name': 'Justin Bieber'}]
+    rst = "Skrillex & Diplo feat. Justin Bieber"
     assert audio_renamer.artists(data) == rst
 
-    
+
 def test_suggestions():
     data = [{'title': 'Hello', 'artists': [{'name': 'Adele'}]},
             {'title': 'Hello (radio edit)', 'artists': [{'name': 'Adele'}]},
-            {'title': 'Where Are Ü Now', 'artists': [{'joinphrase':' & ',
-            'name': 'Skrillex'}, {'joinphrase': ' feat. ','name':'Diplo'},
+            {'title': 'Where Are Ü Now', 'artists':
+             [{'joinphrase':' & ', 'name': 'Skrillex'},
+              {'joinphrase': ' feat. ', 'name':'Diplo'},
             {'name': 'Justin Bieber'}]}, {'title': 'Hello', 'artists':
-            [{'name': 'Adele'}]}, {'title': 'Cry Baby', 'artists': [{'name': 
-            'Melanie Martinez'}]}]
+            [{'name': 'Adele'}]}, {'title': 'Cry Baby', 'artists':
+            [{'name': 'Melanie Martinez'}]}]
 
     names = {0: 'Hello.mp3',
              1: 'Hello (radio edit).mp3',
              2: 'Where Are Ü Now.mp3',
              3: 'Hello.mp3'}
-    
+
     printer = ['(0): Hello.mp3',
                '(1): Hello (radio edit).mp3',
                '(2): Where Are Ü Now.mp3',
@@ -40,30 +47,28 @@ def test_suggestions():
              1: 'Adele - Hello (radio edit).mp3',
              2: 'Skrillex & Diplo feat. Justin Bieber - Where Are Ü Now.mp3',
              3: 'Adele - Hello.mp3'}
-    
+
     printer = ['(0): Adele - Hello.mp3',
                '(1): Adele - Hello (radio edit).mp3',
                '(2): Skrillex & Diplo feat. Justin Bieber - Where Are Ü Now.mp3',
                '(3): Adele - Hello.mp3', "(9): 'SKIP THIS FILE'"]
     assert audio_renamer.suggestions(data, False) == (names, printer)
 
-    
+
 def test_user_input():
     file_name = "fakename"
     options = {0 : "name0",
                1 : "name1",
-               2 : "name2" }
+               2 : "name2"}
 
     input_values = [0, 1, 2, 9]
     output = [("name0", "'fakename' RENAMED AS 'name0'\n"),
               ("name1", "'fakename' RENAMED AS 'name1'\n"),
               ("name2", "'fakename' RENAMED AS 'name2'\n"),
               (False, "Skipped 'fakename'\n")]
-    
+
     def mock_input():
-        return input_values.pop(0)    
+        return input_values.pop(0)
     for i, _ in enumerate(output):
-        audio_renamer.getch.getch=mock_input    
+        audio_renamer.getch.getch = mock_input
         assert audio_renamer.user_input(file_name, options) == output[i]
-
-
